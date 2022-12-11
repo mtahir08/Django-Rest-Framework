d71059dc9bf8d5fed6cad46aab8e10c0f4e14e45
Muhammad Tahir <mtahir.ps@gmail.com>
2022-12-11 17:15:49 +0500

    add decorator

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
 from products.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
 
     model_data = Product.objects.all().order_by("?").first()
@@ -8,11 +11,6 @@ def api_home(request, *args, **kwargs):
     data = {}
 
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-        
- 
+        data = model_to_dict(model_data)
 
-    return JsonResponse(data)
+    return Response(data)
