88020dc72158bea7e4db20a4f5fb97e8f9b18aa5
Muhammad Tahir <mtahir.ps@gmail.com>
2022-12-11 16:53:17 +0500

    return model data

--- api/views.py
+++ api/views.py
@@ -1,20 +1,18 @@
-import json
 from django.http import JsonResponse
-
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
-    # request -> HTTPRequest -> Django
-    # print(request.GET)
-    # print(request.POST)
-    body = request.body # byte string of JSON data
+
+    model_data = Product.objects.all().order_by("?").first()
+
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    # data['headers'] = request.headers # request.META
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    # data['content_type'] = dict(request.content_type)
+
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+        
+ 
 
     return JsonResponse(data)
