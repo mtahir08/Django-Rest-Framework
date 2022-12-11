8dcc7f93cf82a25cd18754d1048dbf305d2c01fb
Muhammad Tahir <mtahir.ps@gmail.com>
2022-12-11 14:48:36 +0500

    params

--- api/views.py
+++ api/views.py
@@ -4,14 +4,17 @@ from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
     # request -> HTTPRequest -> Django
-    print(request.GET)
-    print(request.POST)
+    # print(request.GET)
+    # print(request.POST)
     body = request.body # byte string of JSON data
     data = {}
     try:
         data = json.loads(body)
     except:
         pass
-    print(data)
     # data['headers'] = request.headers # request.META
-    return JsonResponse({ "message" : "HI THERE: this is your Django API response"})
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    # data['content_type'] = dict(request.content_type)
+
+    return JsonResponse(data)
