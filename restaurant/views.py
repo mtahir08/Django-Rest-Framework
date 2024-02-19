a022a531c48e3f006a70b06d440f7a7094247fdb
Muhammad Tahir <mtahir.ps@gmail.com>
2024-02-19 23:36:10 +0500

    fix: changes commit

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,16 +1,10 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
-from django.core import serializers
 from .models import Booking, Menu
-from datetime import datetime
-import json
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
 from rest_framework.viewsets import ModelViewSet
 from .serializers import BookingSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import MenuSerializer
-from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
@@ -23,20 +17,20 @@ def about(request):
 
 
 class BookingViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    permission_classes = [IsAuthenticated]
 
 class MenuItemView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
-    permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 # class UserViewSet(viewsets.ModelViewSet):
+#    permission_classes = [permissions.IsAuthenticated]
 #    queryset = User.objects.all() 
 #    serializer_class = UserSerializer
-#    permission_classes = [permissions.IsAuthenticated]
